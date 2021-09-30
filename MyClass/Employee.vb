bf7c355b2721c8c9216c1cc65fc262caebcc86e6
Lee Joey Noel <70347547+LeeLee-00@users.noreply.github.com>
2021-09-30 08:24:53 -0400

    Add files via upload

---
+++ MyClass/Employee.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Drawing
+
+Public Class Employee
+    ' DRAWING SURFACE
+    Private G As Graphics
+    Private PhotoBox As New Rectangle(5, 5, 150, 200)
+    Private InfoBox As New Rectangle(160, 5, 250, 200)
+
+    ' EMPLOYEE PROPERTIES
+    Public FirstName As String
+    Public LastName As String
+    Public Department As String
+    Public Notes As String
+    Public Status As EmployeeStatus
+
+    'EMPLOYEE PHOOTO
+    Public Photo As Image
+
+    Public Enum EmployeeStatus
+        Active
+        Terminated
+    End Enum
+
+    Public Sub New(GFX As Graphics, FName As String, LName As String, Dept As String, EmpNotes As String, EmpStatus As Integer, Optional EmpPhoto As Image = Nothing)
+        G = GFX
+        FirstName = FName
+        LastName = LName
+        Department = Dept
+        Notes = EmpNotes
+
+        Select Case EmpStatus
+            Case 0
+                Status = EmployeeStatus.Active
+            Case 1
+                Status = EmployeeStatus.Terminated
+            Case Else
+                Status = EmployeeStatus.Active
+        End Select
+        Photo = EmpPhoto
+    End Sub
+
+    Public Sub Draw()
+        ' Clear Drawing Surface
+        G.Clear(Color.FromName("Control"))
+
+        ' Employee Photo Box
+        G.FillRectangle(Brushes.AliceBlue, PhotoBox)
+        G.DrawRectangle(Pens.Black, PhotoBox)
+
+        If Photo IsNot Nothing Then
+            G.DrawImage(Photo, New Rectangle(PhotoBox.X + 1, PhotoBox.Y + 1, PhotoBox.Width - 1, PhotoBox.Height - 1))
+        End If
+
+        'Employee Information
+        G.FillRectangle(Brushes.Lavender, InfoBox)
+        G.DrawRectangle(Pens.Black, InfoBox)
+
+        G.DrawString("" &
+            "Employee Name: " & FirstName & " " & LastName & vbCrLf &
+            "Employee Status: " & Status.ToString & vbCrLf &
+            "Department: " & Department & vbCrLf &
+            "Notes: " & Notes, New Font("Tahoma", 9), Brushes.Black, New Point(InfoBox.X + 5, InfoBox.Y + 5))
+
+    End Sub
+End Class
---
+++ MyClass/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim MyEmployee As Employee
+    Dim PhotoPath As String = ""
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        CreateEmployee("Lee", "John", "Administration", "This dude is the best!", 0)
+    End Sub
+
+    Private Sub CreateEmployee(Fname As String, LName As String, Dept As String, Notes As String, Status As Integer)
+        If MsgBox("Would you like to add a photo?", MsgBoxStyle.YesNo, "Add Photo") = MsgBoxResult.Yes Then
+            OFDPhoto.ShowDialog()
+        End If
+
+        If PhotoPath = "" Then
+            MyEmployee = New Employee(Me.CreateGraphics, Fname, LName, Dept, Notes, Status)
+        Else
+            Dim oImg As Image = Image.FromFile(PhotoPath)
+            MyEmployee = New Employee(Me.CreateGraphics, Fname, LName, Dept, Notes, Status, oImg)
+        End If
+
+        If MyEmployee IsNot Nothing Then
+            MyEmployee.Draw()
+        End If
+    End Sub
+
+    Private Sub OFDPhoto_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OFDPhoto.FileOk
+        PhotoPath = OFDPhoto.FileName
+    End Sub
+
+End Class
